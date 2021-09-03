39307aea9d8044e6f2dab3d1ffd4c2866e76ec01
hapymelz <89086928+hapymelz@users.noreply.github.com>
2021-09-03 09:19:56 -0600

    added values to data members.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,9 +1,22 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class MonsterController
 {
+	private MarshmallowMonster myMonster;
+	
+	public MonsterController()
+	{
+		this.myMonster = new MarshmallowMonster();
+		
+	}
+	
 	public void start()
 	{
+		System.out.print("I made a mosnster!");
+		System.out.print(myMonster.getName() + " is its name!");
+		
 		
 	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -14,11 +14,27 @@ public class MarshmallowMonster
 
 	public MarshmallowMonster()
 	{
-		
+		this.name = "Charlie";
+		this.eyeCount = 3;
+		this.hair = 15;
+		this.legCount = 4;
+		this.teeth = true;
+		this.hasNose = true;
+		this.hasTail = true;
+		this.cutenessQuotient = adorableDeathPercent * 5;
+		this.adorableDeathPercent = eyeCount * 4;
 	}
 	
-	public String getName()
-	{
-		return this.name;
+	public MarshmallowMonster(String name, int eyeCount, int hair, int legCount, boolean teeth, boolean hasNose, boolean hasTail, double cutenessQuotient, double adorableDeathPercent)
+	{ 		
+		this.name = name;
+		this.eyeCount = eyeCount;
+		this.hair = hair;
+		this.legCount = legCount;
+		this.teeth = teeth;
+		this.hasNose = hasNose;
+		this.hasTail = hasTail;
+		this.cutenessQuotient = cutenessQuotient;
+		this.adorableDeathPercent = adorableDeathPercent;
 	}
 }
