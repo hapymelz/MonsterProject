21a8f86aaf9885d6e523b36553fc84168ba14090
hapymelz <89086928+hapymelz@users.noreply.github.com>
2021-09-03 08:57:29 -0600

    got the name.

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -2,5 +2,23 @@ package monster.model;
 
 public class MarshmallowMonster
 {
+	private String name;
+	private int eyeCount;
+	private int hair;
+	private int legCount;
+	private boolean teeth;
+	private boolean hasNose;
+	private boolean hasTail;
+	private double cutenessQuotient;
+	private double adorableDeathPercent;
 
+	public MarshmallowMonster()
+	{
+		
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
 }
